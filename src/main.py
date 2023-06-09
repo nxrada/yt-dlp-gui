9ec2ca3eb0a3d21c30e4889fdcf89fa133e4127b
Nick Kammerer <56482517+nxrada@users.noreply.github.com>
2023-06-08 21:38:09 -0400

    Changed structure

--- src/main.py
+++ src/main.py
@@ -1,14 +1,46 @@
 '''
-Main function for yt-dlp-gui
+yt-dlp-gui: simple GUI wrapper for yt-dlp
+hosted at <https://www.github.com/nxrada/yt-dlp-gui/> 
 '''
 import tkinter as tk
-import gui
-
-def main():
-    root = tk.Tk()
-    local_gui = gui.GUI(root)
-    root.mainloop()
+from yt_dlp import YoutubeDL
+from yt_dlp import utils
 
 
-if __name__ == "__main__":
-    main()
+# Downloads video using YoutubeDL
+def download_video(url: str):
+    try:
+        with YoutubeDL() as ydl:
+            ydl.download(url)
+    except utils.DownloadError as e:
+        print(e)
+
+
+root = tk.Tk()
+root.title("Feet to Meters")
+
+mainframe = tk.Frame(root)
+mainframe.grid(column=0, row=0, sticky=("N", "W", "E", "S"), padx=3, pady=3)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = tk.StringVar()
+feet_entry = tk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=("W", "E"))
+
+meters = tk.StringVar()
+tk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=("W", "E"))
+
+tk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky="W")
+
+tk.Label(mainframe, text="feet").grid(column=3, row=1, sticky="W")
+tk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky="E")
+tk.Label(mainframe, text="meters").grid(column=3, row=2, sticky="W")
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
