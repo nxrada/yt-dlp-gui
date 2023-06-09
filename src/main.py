a1e9758a6781721d146d6ebb60e3bfb2f305a99b
Nick Kammerer <56482517+nxrada@users.noreply.github.com>
2023-06-09 17:14:50 -0400

    Restructured files. Added functionality.

--- src/main.py
+++ src/main.py
@@ -5,42 +5,84 @@ hosted at <https://www.github.com/nxrada/yt-dlp-gui/>
 import tkinter as tk
 from yt_dlp import YoutubeDL
 from yt_dlp import utils
+import re
+
+DOWNLOAD_DIR = "../downloads/%(title)s.%(ext)s"
 
 
-# Downloads video using YoutubeDL
-def download_video(url: str):
+# Sanatizes error before print to GUI. 
+def sanitize_error(e):
+    error = str(e)
+    match = re.search(r"'(.*?)' is not a valid URL.", error)
+    if match:
+        sanitized_string = f"ERROR: {match.group(0)}"
+        return sanitized_string
+    else:
+        return e
+def set_ydl_options():
+    ydl_options = {
+        'outtmpl': DOWNLOAD_DIR,
+    }
+    return ydl_options
+
+def download_video(url: str, console_log):
     try:
-        with YoutubeDL() as ydl:
-            ydl.download(url)
+        # Re-enable the console log, insert text, then disable it
+        console_log['state'] = 'normal'
+        console_log.delete('1.0', tk.END)
+        console_log['state'] = 'disabled'
+        
+        # Download video
+        YoutubeDL(set_ydl_options()).download([url])        
+        
+        # Re-enable the console log, insert text, then disable it
+        console_log['state'] = 'normal'
+        console_log.insert(tk.END, '\nFile downloaded to ../downloads/.')
+        console_log['state'] = 'disabled'
+
     except utils.DownloadError as e:
-        print(e)
+        # Print to console log window
+        console_log['state'] = 'normal'
+        console_log.insert(tk.END, sanitize_error(e)+'\n')
+        console_log['state'] = 'disabled'
 
+# Main function
+def main():
+    # Sets up main application window, with title "yt-dlp"
+    root = tk.Tk()
+    root.title("yt-dlp")
 
-root = tk.Tk()
-root.title("Feet to Meters")
+    # Creates content frame to hold widgets of user interface
+    mainframe = tk.Frame(root)
+    mainframe.grid(column=0, row=0, sticky=('N', 'W', 'E', 'S'), padx=3, pady=3)
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
 
-mainframe = tk.Frame(root)
-mainframe.grid(column=0, row=0, sticky=("N", "W", "E", "S"), padx=3, pady=3)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+    # Widgets for URL input 
+    url = tk.StringVar()
+    url_entry = tk.Entry(mainframe, width=43, textvariable=url)
+    url_entry.grid(column=1, row=1, sticky=('W', 'E'))
+    tk.Label(mainframe, text="Input URL:").grid(column=0, row=1, sticky='E')
 
-feet = tk.StringVar()
-feet_entry = tk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=("W", "E"))
+    # Widget for download button
+    download_button = tk.Button(mainframe, text="Download", command=lambda: download_video(str(url.get()), console_log))
+    download_button.grid(column=2, row=1, sticky='W')
 
-meters = tk.StringVar()
-tk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=("W", "E"))
+    # Widgets for Console log
+    console_log = tk.Text(mainframe, width=60, height=10)
+    console_log['state'] = 'disabled'
+    console_log.grid(column=0, row=2, columnspan=3, sticky='WE')
 
-tk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky="W")
+    # Padding forchildren
+    for child in mainframe.winfo_children(): 
+        child.grid_configure(padx=5, pady=5)
+    
+    # Directs input focus to URL entry bar
+    url_entry.focus()
+    
+    # Runs tk event loop 
+    root.mainloop()
 
-tk.Label(mainframe, text="feet").grid(column=3, row=1, sticky="W")
-tk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky="E")
-tk.Label(mainframe, text="meters").grid(column=3, row=2, sticky="W")
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind("<Return>", calculate)
-
-root.mainloop()
+# Executes code. 
+if __name__ == "__main__":
+    main()
